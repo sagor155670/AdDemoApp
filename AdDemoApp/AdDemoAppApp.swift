4239b8f9c67735a1b16c53218a9dd8c773d53381
sagor155670 <sagor@braincraftapps.com>
2023-10-17 11:53:28 +0600

    Initial Commit

---
+++ AdDemoApp/AdDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdDemoAppApp.swift
+//  AdDemoApp
+//
+//  Created by Saiful Islam Sagor on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AdDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdDemoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AdDemoApp
+//
+//  Created by Saiful Islam Sagor on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
